d78e5e5711a834ff4f8ee9110a650dacf1d92d1d
jegood78 <jegood78@gmail.com>
2022-09-09 14:06:08 -0400

    Updated categories in Expenses.xlsx
    subsetted data into Expenses, Income, and Investements

--- MonthlyExpensesSummary.R
+++ MonthlyExpensesSummary.R
@@ -13,3 +13,13 @@ names(bank) <- tolower(names(bank))
 #change date from date-time to date
 bank$date <- as.Date(bank$date)
 
+#subset the data into pay, expenses, and investments
+categories <- distinct(bank,category)
+categories$category
+
+income <- bank %>% filter(category %in% c("Jeff_Pay","Miscellaneous_Income"))
+
+investments <- bank %>% filter(category %in% c("Investment_Account"))
+
+expenses <- bank %>% filter(!category %in% c("Jeff_Pay","Miscellaneous_Income","Investment_Account"))
+
