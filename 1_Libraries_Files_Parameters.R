b8fd848ad2674897d40e321c78ddd2dd563e4697
jegood78 <jegood78@gmail.com>
2023-02-18 16:44:12 -0500

    Created a file to hold all of the libraries, file loads, and study parameters

---
+++ 1_Libraries_Files_Parameters.R
@@ -0,0 +1,14 @@
+#libraries
+library(tidyverse)
+library(scales)
+library(lubridate)
+library(readxl)
+
+#load files
+net_worth <- read_csv("/users/jeffgood/Desktop/R_Studio_Projects/Financial_Planning/NetWorth.csv")
+
+#parameters
+current_age <- 44
+end_age <- 95
+num_years <- end_age - current_age
+
