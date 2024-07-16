d80afa3a2862def5212330bf945337d3948c5397
jegood78 <jegood78@gmail.com>
2024-07-16 06:26:46 -0400

    new look at finances

--- FI_Calculations_V5.R
+++ FI_Calculations_V5.R
@@ -8,12 +8,23 @@ library(readxl)
 library(writexl)
 
 #~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~
+# set constants
+#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~
+#tsp monthly
+tsp_pre_032024 <- 2285
+tsp_post_032024 <- 1942
+
+#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~
 # banking files
 #~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~
 
 #~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~
 #checking
 checking_raw <- read_csv("/users/jeffgood/Desktop/R_Studio_Projects/Financial_Planning/usaa_checking_raw.csv")
+
+#keep only posted transactions
+checking_raw <- checking_raw %>%
+  filter(Status == "Posted")
 
 #create a key
 checking_raw <- checking_raw %>%
@@ -38,6 +49,10 @@ checking_raw <- checking_raw %>%
 #savings
 savings_raw <- read_csv("/users/jeffgood/Desktop/R_Studio_Projects/Financial_Planning/usaa_savings_raw.csv")
 
+#keep only posted transactions
+savings_raw <- savings_raw %>%
+  filter(Status == "Posted")
+
 #create a key
 savings_raw <- savings_raw %>%
   mutate(key = paste0(Description,"_",Amount,"_",Date))
@@ -56,6 +71,10 @@ savings_raw <- savings_raw %>%
 #~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~
 #visa
 visa_raw <- read_csv("/users/jeffgood/Desktop/R_Studio_Projects/Financial_Planning/usaa_visa_raw.csv")
+
+#keep only posted transactions
+visa_raw <- visa_raw %>%
+  filter(Status == "Posted")
 
 #create a key
 visa_raw <- visa_raw %>%
@@ -81,6 +100,15 @@ bank <- rbind(checking_raw,
 #check duplicates by key
 bank %>% group_by(key) %>% summarise(count = n()) %>% arrange(desc(count))
 
+#remove duplicates
+bank <- distinct(bank)
+
+#check duplicates by 
+bank %>% group_by(key) %>% summarise(count = n()) %>% arrange(desc(count))
+
+#export the new historicals
+write_csv(bank, "/users/jeffgood/Desktop/R_Studio_Projects/Financial_Planning/usaa_historical.csv")
+
 #get rid of spaces in names
 names(bank) <- sub(" ","_",names(bank))
 
@@ -94,8 +122,70 @@ bank$category <- gsub(" ","_",bank$category)
 bank$category <- gsub("/","_",bank$category)
 bank$category <- tolower(bank$category)
 
-#export the new historicals
-write_csv(bank, "/users/jeffgood/Desktop/R_Studio_Projects/Financial_Planning/usaa_formatted.csv")
-
 #group by month and transaction typ
-bank %>% group_by(month, transaction_type) %>% summarise(monthly_amount = sum(amount))
+bank_monthly_raw <- bank %>% group_by(month, transaction_type) %>% summarise(monthly_amount = sum(amount))
+
+#separate by transaction type to process
+
+#income.  Add TSP amount back and create flow category == "in"
+income_monthly <- bank_monthly_raw %>%
+  filter(transaction_type == "income") %>%
+  mutate(flow = "in",
+         monthly_amount = if_else(month <= "2024-03",
+                                  monthly_amount + tsp_pre_032024,
+                                  monthly_amount + tsp_post_032024))
+
+#investments.  make amounts positive, add tsp amounts and create flow category == "out
+investment_monthly <- bank_monthly_raw %>%
+  filter(transaction_type == "investment") %>%
+  mutate(flow = "out",
+         monthly_amount = if_else(month <= "2024-03",
+                                  -1 * monthly_amount + tsp_pre_032024,
+                                  -1 * monthly_amount + tsp_post_032024))
+
+#fixed expenses. make amounts positive and create flow category == "out"
+fixed_expenses_monthly <- bank_monthly_raw %>%
+  filter(transaction_type == "fixed_expenses") %>%
+  mutate(flow = "out",
+         monthly_amount = -1 * monthly_amount)
+
+#misc expenses. make amounts positive and create flow category == "out"
+misc_expenses_monthly <- bank_monthly_raw %>%
+  filter(transaction_type == "misc_expenses") %>%
+  mutate(flow = "out",
+         monthly_amount = -1 * monthly_amount)
+
+#savings. make amounts positive and create flow category == "out"
+savings_monthly <- bank_monthly_raw %>%
+  filter(transaction_type == "savings") %>%
+  mutate(flow = "out")
+
+#put them back together
+bank_monthly <- rbind(income_monthly,
+                      investment_monthly,
+                      savings_monthly,
+                      fixed_expenses_monthly,
+                      misc_expenses_monthly)
+
+#create a visualization
+ggplot(data = bank_monthly,
+       aes(x = month,
+           y = monthly_amount,
+           group = flow,
+           fill = transaction_type)) +
+  geom_col(position = "dodge") +
+  scale_fill_manual(values = c("fixed_expenses" = "red",
+                               "misc_expenses" = "red4",
+                               "savings" = "blue",
+                               "investment" = "skyblue",
+                               "income" = "green4")) +
+  scale_y_continuous(seq(0,25000,500), labels = dollar)+
+  theme_minimal() +
+  labs(title = "Income and Expenses by Month", subtitle = "Last 12 Months", x = NULL, y = NULL) +
+  theme(axis.ticks = element_blank(),
+        axis.title.y = element_blank(),
+        axis.text.x = element_text(angle = -90, vjust = 0),
+        axis.line = element_line(colour = "grey",
+                                 linewidth = 1,
+                                 linetype = "solid"),
+        legend.position = "bottom")
